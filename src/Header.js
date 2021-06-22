01887593a539ded302e90e6679676f119310f30d
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-06-22 12:06:26 -0700

    Beginning Facebook clone project

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+        <div class="header__left">
+
+        </div>
+        <div class="header__middle">
+
+        </div>
+        <div class="header__right">
+            
+        </div>
+    </div>
+    
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDe9iifNDVqckVcn36bmkJrPo06uZsrOY4",
+    authDomain: "fb-clone-591ff.firebaseapp.com",
+    projectId: "fb-clone-591ff",
+    storageBucket: "fb-clone-591ff.appspot.com",
+    messagingSenderId: "285052560160",
+    appId: "1:285052560160:web:36b5d4efa52b6f4fa12d45"
+  };
