00436cff3dc133b0048f5ff6a5d2ac78f8c573e1
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-06-26 11:19:04 -0700

    working on the post message functionality

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
+import Post from './Post';
 import './Feed.css';
 
 
@@ -8,6 +10,27 @@ function Feed() {
 
     <div className="feed">
       <StoryReel />
+      <MessageSender />
+
+      <Post 
+        profilePic='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FUKeI9bdB6Qg%2Fmaxresdefault.jpg&f=1&nofb=1'
+        message='You are more than enough.'
+        timestamp='time to LEVEL UP'
+        username='name game'
+        image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kjLts0G7wpdA-C4Uq__sOAHaLO%26pid%3DApi&f=1'
+      />
+      <Post 
+        profilePic='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP._IuDx0yR6HEyAiP7w-pVKQHaEK%26pid%3DApi&f=1'
+        message='You can do anything you put your mind to.'
+        timestamp='and flex time'
+        username='name blame'
+      />
+      <Post 
+        profilePic='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.buntINTHFLyDODyMKXNYcgHaEK%26pid%3DApi&f=1'
+        message='Reach beyond the limits you know.'
+        timestamp='booom time'
+        username='name shame'
+      />
     </div>
     
   );
---
+++ src/Post.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Avatar } from '@material-ui/core';
+import './Post.css';
+
+
+function Post({ profilePic, image, username, timestamp, message }) {
+  return (
+    <div className="post">
+        <div class="post__top">
+            <Avatar src={profilePic} className="post__avatar"/>
+            <div class="post__topInfo">
+                <h3>{username}</h3>
+                <p>{timestamp}</p>
+            </div>
+        </div>
+
+        <div className="post__bottom">
+            <p>{message}</p>
+        </div>
+        <div className="post__image">
+            <img src={image} alt="" />
+        </div>
+    </div>
+  );
+}
+
+export default Post;
