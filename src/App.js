b32ce0ea5f0a25ac9c313ab3430fbc3cea858392
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-07-01 13:25:16 -0700

    began  google auth login feature

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Feed from './Feed';
 import Widgets from './Widgets';
 import Login from './Login';
 import './Sidebar.css';
+import { useStateValue } from './StateProvider';
 
 function App() {
-  const user = null;
+  const [{ user }, dispatch] = useStateValue();
   return (
     // BEM naming convention
     <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import './Login.css';
+import { auth, provider } from "./firebase";
+import { actionTypes } from "./reducer";
+import { useStateValue } from "./StateProvider";
 
 function Login() {
-    const signIn = () => {
+  const [state, dispatch] = useStateValue();
 
+    const signIn = () => {
+      auth.signInWithPopup(provider)
+      .then(result => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        })
+        console.log(result.user);
+      })
+      .catch((error) => alert(error.message));
     }
   return (
     <div className="login">
