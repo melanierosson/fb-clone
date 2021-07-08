42d94e7c40ba7c230f46ddc5d62c0d859667ed53
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-07-08 14:34:29 -0700

    push before deploy

--- src/Feed.js
+++ src/Feed.js
@@ -10,7 +10,9 @@ function Feed() {
   const [posts, setPost] = useState([]);
 
   useEffect(() => {
-    db.collection('posts').onSnapshot((snapshot) => (
+    db.collection('posts')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => (
       setPost(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
     ))
   }, [])
@@ -23,12 +25,12 @@ function Feed() {
 
       {posts.map(post => (
         <Post 
-          key={post.id}
-          profilePic={post.profilePic}
-          message={post.message}
-          timestamp={post.timestamp}
-          username={post.username}
-          image={post.image}
+          key={post.data.id}
+          profilePic={post.data.profilePic}
+          message={post.data.message}
+          timestamp={post.data.timestamp}
+          username={post.data.username}
+          image={post.data.image}
         />
       ))}
       {/* <Post 
--- src/MessageSender.js
+++ src/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import { useStateValue } from './StateProvider';
+import db from './firebase';
+import firebase from 'firebase';
 
 function MessageSender() {
     const [input, setInput] = useState(``);
@@ -13,6 +15,14 @@ function MessageSender() {
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        db.collection('posts').add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            profilePic: user.photoURL,
+            username: user.displayName,
+            image: imageUrl,
+        })
         // DB stuff
         setInput('');
         setImageUrl('');
